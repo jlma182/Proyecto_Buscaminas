879688fe09bb903ac33a403a927cd2589522f401
brianwolf01 <brianwolf012@hotmail.com>
2020-11-25 23:09:03 -0400

    Cambios realizados en vista de nivel intermedio y personalizado y agregados sus tableros

--- app.rb
+++ app.rb
@@ -21,7 +21,22 @@ post '/formato' do
     # @juga2=params[:nombre]
     erb:condicion
 end
-
+post '/formato2' do
+    # @JugadorX=jugador.getNombre()
+    # erb:condicion
+    jugador=Jugador.new
+    # @juga1=jugador.setNombre((params[:nombre]))
+    # @juga2=params[:nombre]
+    erb:condicion2
+end
+post '/formatoP' do
+    # @JugadorX=jugador.getNombre()
+    # erb:condicion
+    jugador=Jugador.new
+    # @juga1=jugador.setNombre((params[:nombre]))
+    # @juga2=params[:nombre]
+    erb:condicionP
+end
 get '/juego' do
     
     @nombre_usuario=params[:nombre]
@@ -40,7 +55,7 @@ post '/obteneryandx' do
     x=params[:X].to_i
     y=params[:Y].to_i
     @nombre_usuario=params[:Usuario]
-    tablero_juego.cambiar_estado_celda(x,y)
+    tablero_juego.cambiar_estado_celda(x-1,y-1)
     #tablero_juego.verificar_filas_x_columnas(x,y)
     # if(verificar_caracter==false)    
     @mostrar_tablero=tablero_juego.tablero
@@ -53,16 +68,32 @@ post '/disminuir_contador_bandera' do
     x=params[:X].to_i
     y=params[:Y].to_i
     @nombre_usuario=params[:Usuario]
-    tablero_juego.cambiar_estado_bandera(x,y)
+    tablero_juego.cambiar_estado_bandera(x-1,y-1)
     tablero_juego.disminuir_cantidad_banderas_en_uno()
     @mostrar_tablero=tablero_juego.tablero
     erb :tablero
 end
 
-post '/formato2' do
-    erb:condicion2
+get '/formato2juego' do
+    @nombre_usuario=params[:nombre]
+    tablero_juego.generar_tablero_con_celda_nivel_intermedio()
+    tablero_juego.insert_minas_aleatoreamente(40)
+    tablero_juego.precargar_numeros()
+    @mostrar_tablero=tablero_juego.tablero
+    
+    erb :tablero
 end
 
-post '/formatoP' do
-    erb:condicionP
+post '/formatoPjuego' do
+    @nombre_usuario=params[:nombre]
+    @filas=params[:fila]
+    @columnas=params[:columna]
+    @minas=params[:mina]
+    @banderas=params[:bandera]
+    #tablero_juego.generar_tablero_con_celda_nivel_intermedio()
+    #tablero_juego.insert_minas_aleatoreamente(40)
+    #tablero_juego.precargar_numeros()
+    @mostrar_tablero=tablero_juego.tablero
+    
+    erb :tablero
 end
