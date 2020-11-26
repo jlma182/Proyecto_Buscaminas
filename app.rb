265d8c67612ef83fe2f71082fc2a8e2ca301ca29
brianwolf01 <brianwolf012@hotmail.com>
2020-11-26 00:06:25 -0400

    Añadido el contador de banderas disponibles y correcion en la entrada a los niveles de los tableros

--- app.rb
+++ app.rb
@@ -47,6 +47,7 @@ get '/juego' do
     tablero_juego.insert_minas_aleatoreamente(10)
     tablero_juego.precargar_numeros()
     @mostrar_tablero=tablero_juego.tablero
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
     
     erb :tablero
 end
@@ -59,6 +60,7 @@ post '/obteneryandx' do
     #tablero_juego.verificar_filas_x_columnas(x,y)
     # if(verificar_caracter==false)    
     @mostrar_tablero=tablero_juego.tablero
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
     erb :tablero 
     # else
     # erb :fin_juego(nombre_j,tiempo) 
@@ -71,7 +73,28 @@ post '/disminuir_contador_bandera' do
     tablero_juego.cambiar_estado_bandera(x-1,y-1)
     tablero_juego.disminuir_cantidad_banderas_en_uno()
     @mostrar_tablero=tablero_juego.tablero
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
     erb :tablero
+end
+post '/disminuir_contador_bandera_2' do
+    x=params[:X].to_i
+    y=params[:Y].to_i
+    @nombre_usuario=params[:Usuario]
+    tablero_juego.cambiar_estado_bandera(x-1,y-1)
+    tablero_juego.disminuir_cantidad_banderas_en_uno()
+    @mostrar_tablero=tablero_juego.tablero
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
+    erb :tablero_intermedio
+end
+post '/disminuir_contador_bandera_P' do
+    x=params[:X].to_i
+    y=params[:Y].to_i
+    @nombre_usuario=params[:Usuario]
+    tablero_juego.cambiar_estado_bandera(x-1,y-1)
+    tablero_juego.disminuir_cantidad_banderas_en_uno()
+    @mostrar_tablero=tablero_juego.tablero
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
+    erb :tablero_personalizado
 end
 
 get '/formato2juego' do
@@ -80,7 +103,7 @@ get '/formato2juego' do
     tablero_juego.insert_minas_aleatoreamente(40)
     tablero_juego.precargar_numeros()
     @mostrar_tablero=tablero_juego.tablero
-    
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
     erb :tablero_intermedio
 end
 
@@ -94,6 +117,6 @@ get '/formatoPjuego' do
     tablero_juego.insert_minas_aleatoreamente(minas)
     tablero_juego.precargar_numeros()
     @mostrar_tablero=tablero_juego.tablero
-    
+    @banderas_disponibles=tablero_juego.getCantidad_banderas()
     erb :tablero_personalizado
 end
