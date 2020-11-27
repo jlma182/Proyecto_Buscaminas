c0cec8dbb81f4bbf1ac4f8f9d0987da64e24a2cf
brianwolf01 <brianwolf012@hotmail.com>
2020-11-27 00:25:09 -0400

    Eliminacion de codigo sucio y refactor de test

--- app.rb
+++ app.rb
@@ -14,36 +14,19 @@ post '/escogerNivel' do
 end
 
 post '/formato' do
-    # @JugadorX=jugador.getNombre()
-    # erb:condicion
-    jugador=Jugador.new
-    # @juga1=jugador.setNombre((params[:nombre]))
-    # @juga2=params[:nombre]
+    
     erb:condicion
 end
 post '/formato2' do
-    # @JugadorX=jugador.getNombre()
-    # erb:condicion
-    jugador=Jugador.new
-    # @juga1=jugador.setNombre((params[:nombre]))
-    # @juga2=params[:nombre]
     erb:condicion2
 end
 post '/formatoP' do
-    # @JugadorX=jugador.getNombre()
-    # erb:condicion
-    jugador=Jugador.new
-    # @juga1=jugador.setNombre((params[:nombre]))
-    # @juga2=params[:nombre]
     erb:condicionP
 end
 get '/juego' do
     
     @nombre_usuario=params[:nombre]
-    #jugador.setNombre(@nombre_usuario)
-    #tablero_juego.generar_tablero_numerico()
     tablero_juego.generar_tablero_con_celda_nivel_basico()
-    #tablero_juego.generar_tablero_con_minas_con_celda()
     tablero_juego.insert_minas_aleatoreamente(10)
     tablero_juego.precargar_numeros()
     @mostrar_tablero=tablero_juego.tablero
@@ -57,13 +40,9 @@ post '/obteneryandx' do
     y=params[:Y].to_i
     @nombre_usuario=params[:Usuario]
     tablero_juego.cambiar_estado_celda(x-1,y-1)
-    #tablero_juego.verificar_filas_x_columnas(x,y)
-    # if(verificar_caracter==false)    
     @mostrar_tablero=tablero_juego.tablero
     @banderas_disponibles=tablero_juego.getCantidad_banderas()
     erb :tablero 
-    # else
-    # erb :fin_juego(nombre_j,tiempo) 
 end
 
 post '/disminuir_contador_bandera' do
